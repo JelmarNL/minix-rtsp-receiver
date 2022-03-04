8ac21455366cf1db81e68e69ecb1c9452519835b
Jelmar <33655959+JelmarNL@users.noreply.github.com>
2022-03-04 13:05:14 +0100

    Added logger

---
+++ src/main/java/me/JelmarNL/minixRtspReceiver/Logger.java
@@ -0,0 +1,24 @@
+package me.JelmarNL.minixRtspReceiver;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logger {
+    public static void info(String module, String text) {
+        System.out.println(getTimeStamp() + "[INFO][" + module + "]: " + text);
+    }
+    public static void warning(String module, String text) {
+        System.out.println(getTimeStamp() + "[WARN][" + module + "]: " + text);
+    }
+    public static void error(String module, String text) {
+        System.out.println(getTimeStamp() + "[ERRO][" + module + "]: " + text);
+    }
+    
+    public static void debug(String text) {
+        System.out.println(getTimeStamp() + "[INFO][DEBUG]: " + text);
+    }
+    
+    private static String getTimeStamp() {
+        return "[" + new SimpleDateFormat("HH.mm.ss").format(new Date()) + "]";
+    }
+}
