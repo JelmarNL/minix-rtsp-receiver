35591905bed7b3216e0010dc7962899d7cec6c28
Jelmar <33655959+JelmarNL@users.noreply.github.com>
2022-03-23 14:10:55 +0100

    Fixed watchdog image comparison

--- src/main/java/me/JelmarNL/minixRtspReceiver/Video/RtspPlayer.java
+++ src/main/java/me/JelmarNL/minixRtspReceiver/Video/RtspPlayer.java
@@ -201,7 +201,7 @@ public class RtspPlayer extends Thread {
         }
     }
     
-    class Watchdog extends Thread { //TODO: Does an empty room trigger this?
+    public class Watchdog extends Thread { //TODO: Does an empty room trigger this?
         private BufferedImage oldImage = null;
         private boolean running = true;
         
@@ -216,7 +216,7 @@ public class RtspPlayer extends Thread {
                         //Just try again
                         continue;
                     }
-                    if (newImage.equals(oldImage)) {
+                    if (bufferedImagesEqual(newImage, oldImage)) {
                         //Restart stream
                         Logger.warning("RtspWatchdog", "Frozen stream has been detected, restarting...");
                         Platform.runLater(() -> Player.embeddedMediaPlayer.media().play(createStreamUrl(), getStreamOptions()));
@@ -243,6 +243,21 @@ public class RtspPlayer extends Thread {
                 Logger.warning("RtspWatchdog", "Failed to verify is stream is running.");
             }
             return null;
+        }
+
+        boolean bufferedImagesEqual(BufferedImage img1, BufferedImage img2) {
+            if (img1.getWidth() == img2.getWidth() && img1.getHeight() == img2.getHeight()) {
+                for (int x = 0; x < img1.getWidth(); x++) {
+                    for (int y = 0; y < img1.getHeight(); y++) {
+                        if (img1.getRGB(x, y) != img2.getRGB(x, y)) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
